9c8f94876e64dc7d98b31b267243dfaaefecf902
Pritom <pritamparashar8@gmail.com>
2022-08-24 17:06:42 +0530

    2nd commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+
 app.use(userRouter);
 
 const port = process.env.PORT || 8000;
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema(
   {
@@ -13,11 +15,57 @@ const userSchema = new mongoose.Schema(
       required: true,
       minlength: 8,
     },
+    tokens: [
+      {
+        token: {
+          type: "string",
+          required: true,
+        },
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign(
+    { _id: user.id.toString() },
+    process.env.NOTER_JWT_SECRET
+  );
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (username, password) => {
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    throw new Error("Unable to Login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to Login");
+  }
+
+  return user;
+};
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
 
 const user = mongoose.model("User", userSchema);
 
